a23107751238fecef99a4905c539ac2b3e0ec825
YASSINE <contact@yassine.fun>
2024-07-23 13:32:02 +0000

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // This promise doesn't do anything specific, it just resolves immediately
+    resolve();
+  });
+}
